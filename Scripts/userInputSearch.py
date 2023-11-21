4d6194b6de543343a33aaccb7f5fe10a3e7dba2d
rmmott <rmmott@ncsu.edu>
2023-11-21 11:40:10 -0500

    Update userInputSearch.py

--- Scripts/userInputSearch.py
+++ Scripts/userInputSearch.py
@@ -1,19 +1,65 @@
-#date and company searcher
+#search script
 
 import numpy as np
 import pandas as pd
 import math
 
+##############################################################################
 #user inputs
+#this will ultimately structured as a function with 12 inputs
+##############################################################################
+
 begin_day = 1
-begin_month = 10
+begin_month = 1
 begin_year = 2013
 end_day = 31
 end_month = 4
-end_year = 2017
-assignee_search = 'TERYX'
+end_year = 2014
+assignee_search = ''
+inventor_search = ''
+country_search = ''
+state_search = ''
+city_search = ''
+cpc_search = 'C12N'
 
-#date calculation
+##############################################################################
+#assign logic parameters for search for use in if/then statements later
+##############################################################################
+
+if assignee_search == '':
+   assignee_search_log = 'False'
+else:
+   assignee_search_log = 'True'
+    
+if inventor_search == '':
+   inventor_search_log = 'False'
+else:
+   inventor_search_log = 'True'
+
+if country_search == '':
+   country_search_log = 'False'
+else:
+   country_search_log = 'True'
+
+if state_search == '':
+   state_search_log = 'False'
+else:
+   state_search_log = 'True'
+
+if city_search == '':
+   city_search_log = 'False'
+else:
+   city_search_log = 'True'
+
+if cpc_search == '':
+   cpc_search_log = 'False'
+else:
+   cpc_search_log = 'True'
+
+###############################################################################
+#create array of dates based on the specified date range by finding ALL dates
+#that were between 2013 and 2023, and then taking only the ones specified
+###############################################################################
 
 begin_date = str(begin_year) + '-' + str(begin_month) + '-' + str(begin_day) 
 end_date = str(end_year) + '-' + str(end_month) + '-' + str(end_day) 
@@ -54,27 +100,82 @@ for w in dates:
 
 date_search = dates[begin_index:end_index]
 
+###############################################################################
+#separate the numbers of years searched for the purpose of reading .csv files
+###############################################################################
 
 year_range = end_year - begin_year
 years_mat = np.linspace(begin_year, end_year, year_range+1, dtype=int)
 response_query = []
 
 for y in years_mat:
-    # Returns a TextFileReader, which is iterable with chunks of 1000 rows.
+    
+###############################################################################
+#for each .csv, read in chunks of 10,000 rows at a time, for memory management
+###############################################################################
+    
+    # Returns a TextFileReader, which is iterable with chunks of 10000 rows.
     filename = 'patents_' + str(y) + '.csv'
-    csv_iterator = pd.read_csv(filename, iterator=True, chunksize=1000)
+    csv_iterator = pd.read_csv(filename, iterator=True, chunksize=10000)
     
     # Iterate through the dataframe chunks
     for chunk in csv_iterator:
         for index, row in chunk.iterrows():
+
+###############################################################################
+#if a given row is responsive to the date range, check fields for hits
+#(some of the compliations below stem from the fact that when the data value
+#is empty, Python runs into problems, so you have to do all these checks
+#before you make an improper comparison (for example, whether a string is in
+#a NaN)
+###############################################################################
+    
+            
             if row['patent_date'] in date_search:
-                assignee_org = row['assignee_org_name']
-                if isinstance(assignee_org, str):
-                    if assignee_search in assignee_org:
-                        response_query.append(row['patent_abstract'])
+                
+                assignee = row['assignee_org_name']
+                inventor = row['inventor_name']
+                country = row['inventor_country']
+                state = row['inventor_state']
+                city = row['inventor_city']
+                cpc = row['cpc_group_id']    
+                
+                if assignee_search_log == 'False' or (assignee_search_log == 'True' and isinstance(assignee, str) and assignee_search in assignee):                    
+                   
+                    if inventor_search_log == 'False' or (inventor_search_log == 'True' and isinstance(inventor, str) and inventor_search in inventor):
+                       
+                        if country_search_log == 'False' or (country_search_log == 'True' and isinstance(country, str) and country_search in country):
+                          
+                            if state_search_log == 'False' or (state_search_log == 'True' and isinstance(state, str) and state_search in state):
+                              
+                                if city_search_log == 'False' or (city_search_log == 'True' and isinstance(city, str) and city_search in city):
+                                   
+                                    if cpc_search_log == 'False' or (cpc_search_log == 'True' and isinstance(cpc, str) and cpc_search in cpc):
+
+###############################################################################
+#if a given row is responsive, append the abstract in new array
+###############################################################################                                    
+
+                                        response_query.append(row['patent_abstract'])
+                                    
+                                    else:
+                                        pass
+                                else:
+                                    pass
+                            else:
+                                pass
+                        else:
+                            pass
                     else:
                         pass
                 else:
                     pass
             else:
                 pass
+            
+###############################################################################
+#depending on the size of the search, the response_query may vary in size
+#from in the single digits to the thousands or tends of thousands
+###############################################################################
+
+
