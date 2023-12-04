2b1364a4173a1f72a402a6c3c51ab4cab635a2bd
bsrogers <bsrogers@ncsu.edu>
2023-12-04 08:42:35 -0500

    Add files via upload

---
+++ Scripts/TFIDF/Dtype.py
@@ -0,0 +1,22 @@
+# Explicit data types for when loading large csv files to dataframe.
+
+dtype = {
+        "patent_id": "string",
+        "patent_title": "string",
+        "patent_date": "string",
+        "patent_abstract": "string",
+        "application_id": "string",
+        "application_filing_date": "string",
+        "applicant_name": "string",
+        "applicant_organization": "string",
+        "assignee_name": "string",
+        "assignee_org_name": "string",
+        "cpc_class_id": "string",
+        "cpc_subclass_id": "string",
+        "cpc_group_id": "string",
+        "filing_date": "string",
+        "inventor_name": "string",
+        "inventor_city": "string",
+        "inventor_state": "string",
+        "inventor_count": "string"
+    }
